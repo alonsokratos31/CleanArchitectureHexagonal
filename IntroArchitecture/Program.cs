c67e458139e400562dd5d3dd4e60eda4506d42c7
alonsokratos31 <alonso.garcia025@gmail.com>
2025-05-23 23:06:55 -0400

    Se continua Curso

---
+++ IntroArchitecture/Program.cs
@@ -0,0 +1,101 @@
+﻿
+using Microsoft.Extensions.DependencyInjection;
+
+var services = new ServiceCollection();
+services.AddTransient<IRepository, NamesUnique>();
+services.AddTransient<MySystem>();
+var serviceProvider = services.BuildServiceProvider();
+var mySystem = serviceProvider.GetRequiredService<MySystem>(); 
+mySystem.Run();
+
+
+// Capa Dominio
+// Interfces/Aplicacion
+public interface IRepository
+{
+    void Save(string name);
+    IEnumerable<string> Get();
+}
+
+// Capa Datos
+// Componentes Names
+
+public class Names : IRepository
+{
+    private readonly List<string> _names = new();
+
+    public void Save(string name)
+    {
+        _names.Add(name);
+    }
+
+    public IEnumerable<string> Get() => _names;
+
+}
+
+// Capa Datos
+// Componentes Names
+
+public class NamesUnique : IRepository
+{
+    private readonly HashSet<string> _names = new();
+
+    public void Save(string name)
+    {
+        _names.Add(name);
+    }
+
+    public IEnumerable<string> Get() => _names;
+
+}
+
+
+
+// Capa Presentacion
+// Componente MySystem
+
+public class MySystem
+{
+    private IRepository _repository;
+
+    public MySystem(IRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public void Run()
+    {
+        while (true)
+        {
+            Console.WriteLine("\nSeleccionar una opción:");
+            Console.WriteLine("1 - Agregar un nombre");
+            Console.WriteLine("2 - Mostrar nombres almacenados");
+            Console.WriteLine("3 - Salir");
+            Console.Write("Opción: ");
+
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Ingrese un nombre: ");
+                    string name = Console.ReadLine();
+                    _repository.Save(name);
+                    break;
+                case "2":
+                    Console.WriteLine("\nNombres almacenados:");
+                    foreach (var currentName in _repository.Get())
+                    {
+                        Console.WriteLine($"- {currentName}");
+                    }
+                    break;
+                case "3":
+                    Console.WriteLine("Saliendo del sistema....");
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida, intente de nuevo");
+                    break;
+            }
+        }
+    }
+}
---
+++ SOLID/Program.cs
@@ -0,0 +1,138 @@
+﻿
+IRepository<string> beer = new BeerData();
+beer.Add("Cristal");
+beer.Add("Pilsen");
+var reportGeneratorBeer = new BeerReportGenerate(beer);
+var report = new Report();
+var data = reportGeneratorBeer.Generate();
+report.SaveReport(reportGeneratorBeer, "report.txt");
+
+
+void Show(IReportShow report)
+{
+    report.Show();
+}
+
+public interface IReportGenerator
+{
+    public string Generate();
+
+}
+
+public interface IReportShow
+{
+    public void Show();
+
+}
+
+public interface IRepository<T>
+{
+    public void Add(T item);
+
+    public List<T> Get();
+}
+
+
+public class BeerData :  IRepository<string>
+{
+    protected List<string> _beers;
+
+    public BeerData()
+    {
+        _beers = new List<string>();
+    }
+
+    public virtual void Add(string beer)
+        => _beers.Add(beer);
+
+    public List<string> Get()
+        => _beers;
+}
+
+public class LimitedBeerData
+{
+    private IRepository<string> _beerData;
+    private int _limit;
+    private int _count = 0;
+
+    public LimitedBeerData(int limit, IRepository<string> beerData)
+    {
+        _limit = limit;
+        _beerData = beerData;
+    }
+
+    public void AddBeer(string beer)
+    {
+        if(_count >= _limit)
+        {
+            throw new InvalidOperationException("Limite de cerbezas alcanzado");
+        }
+        _beerData.Add(beer);
+        _count++;
+    }
+
+    public List<string> Get()
+    {
+        return _beerData.Get();
+    }
+}
+
+public class BeerReportGenerate: IReportGenerator, IReportShow
+{
+    private IRepository<string> _beerData;
+    private List<string> _beers;
+    public BeerReportGenerate(IRepository<string> beerData)
+    {
+        _beerData = beerData;
+    }
+    public string Generate()
+    {
+        string data = "";
+        foreach (var beer in _beers)
+        {
+            data += " Cerveza: " + beer + Environment.NewLine;
+        }
+        return data;
+    }
+
+    public void Show()
+    {
+        foreach (var beer in _beers)
+        {
+            Console.WriteLine(" Cerveza: " + beer);
+        }
+    }
+}
+
+
+public class BeerReportGenerateHTML : IReportGenerator
+{
+    private IRepository<string> _beerData;
+    private List<string> _beers;
+    public BeerReportGenerateHTML(IRepository<string> beerData)
+    {
+        _beerData = beerData;
+    }
+    public string Generate()
+    {
+        string data = "";
+        foreach (var beer in _beers)
+        {
+            data += " Cerveza: " + beer + Environment.NewLine;
+        }
+        return data;
+    }
+}
+
+
+public class Report
+{
+    public void SaveReport(IReportGenerator reportGenerator, string filePath)
+    {
+        using(var writer = new StreamWriter(filePath))
+        {
+            string data = reportGenerator.Generate();
+            writer.WriteLine(data);
+        }
+    }
+}
